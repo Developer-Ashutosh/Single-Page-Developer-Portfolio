7e3a559a5a6610b5c3be8a9936286fecc3e7a970
Ashutosh Kumar <kumar.ashutosh.9190@gmail.com>
2023-12-23 21:43:38 +0530

    initial file

---
+++ README.md
@@ -0,0 +1,48 @@
+# Frontend Mentor - Single-page developer portfolio
+
+This is a solution to the [Single-page developer portfolio](https://www.frontendmentor.io/challenges/singlepage-developer-portfolio-bBVj2ZPi-x/hub).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the game depending on their device's screen size
+- Receive an error message when the form is submitted if :
+    - Any field is empty
+    - The email address is not formatted correctly
+- **Bonus 1**: Hook the form up so it sends and stores the user's enquiry (you can use a spreadsheet or Airtable to save the enquiries)
+- **Bonus 2**: Add your own details (image, skills, projects) to replace the ones in the design
+
+### Links
+
+- Solution URL: [Single-page Developer Portfolio Solution](https://github.com/Developer-Ashutosh/Single-page-Developer-Portfolio)
+- Live Site URL: [Single-page Developer Portfolio](https://developer-ashutosh.github.io/Single-page-Developer-Portfolio/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+## Author
+
+- GitHub - [Ashutosh Kumar](https://www.github.com/Developer-Ashutosh/)
+- Frontend Mentor - [@Ashutosh Kuamr](https://www.frontendmentor.io/profile/yourusername)
---
+++ script.js
@@ -0,0 +1,135 @@
+// Function to scroll smoothly to the top
+const scrollWindow = () => {
+    const toTopBtn = document.querySelector('.top-btn');
+
+    document.body.addEventListener('scroll', () => {
+        toTopBtn.style.opacity = (document.body.scrollTop > 110) ? '1' : '0';
+    });
+
+    toTopBtn.addEventListener('click', () => {
+        document.body.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+};
+
+// Function to handle form validation
+const validateForm = () => {
+    const form = document.querySelector('form');
+    const fields = form.querySelectorAll('.field');
+    const sendBtn = form.querySelector('#send-btn');
+
+    sendBtn.addEventListener('click', () => {
+        fields.forEach(field => {
+            const input = field.querySelector('.input');
+            const span = field.querySelector('span');
+
+            if (input.value === '') {
+                handleValidationErrors(input, span, 'This field is required.');
+            } else if (input.id === 'email' && !input.value.includes('@')) {
+                handleValidationErrors(input, span, 'Sorry, invalid email format.');
+            } else {
+                resetValidationState(input, span);
+            }
+        });
+    });
+};
+
+// Helper function to handle validation errors
+const handleValidationErrors = (input, span, errorMessage) => {
+    input.classList.add('error');
+    span.classList.add('error-icon');
+    span.textContent = errorMessage;
+    span.style.display = 'block';
+};
+
+// Helper function to reset validation state
+const resetValidationState = (input, span) => {
+    input.classList.remove('error');
+    span.classList.remove('error-icon');
+    span.style.display = 'none';
+};
+
+// Function to fetch data from JSON files
+const fetchData = async (section, sectionAttr, more = false) => {
+    const response = await fetch(`./JSON Files/${section}.json`);
+    const data = await response.json();
+    const sectionElement = document.querySelector(sectionAttr);
+    sectionElement.innerHTML = '';
+
+    const startIndex = more ? data.length : Math.min(6, data.length);
+
+    data.slice(0, startIndex).forEach(sectionData => {
+        const div = (section === 'skills') ? createSkills(sectionData) : createProjects(sectionData);
+        sectionElement.append(div);
+    });
+
+    toggleMoreBtn(sectionElement, data);
+};
+
+// Function to create skills element
+const createSkills = (skill) => {
+    const { title, experience } = skill;
+    return createDivElement('skills', `
+        <h2 class="skill">${title}</h2>
+        <p class="experience">${experience}</p>`
+    );
+};
+
+// Function to create projects element
+const createProjects = (project) => {
+    const { title, thumbnail, languages, links } = project;
+    return createDivElement('project', `
+        <div class="project-view">
+            <img src="${thumbnail}" alt="${title}">
+            <div class="project-links">
+                <a target="_blank" href="${links.project}" class="view-project btn">View Project</a>
+                <a target="_blank" href="${links.code}" class="view-code btn">View Code</a>
+            </div>
+        </div>
+        <h3 class="project-title">${title}</h3>
+        <div class="project-stack-tags">
+           ${languages.map(e => `<span class="project-stack-tag">${e}</span>`).join('')}
+        </div>
+        <div class="project-links-visible">
+            <a target="_blank" href="${links.project}" class="view-project btn">View Project</a>
+            <a target="_blank" href="${links.code}" class="view-code btn">View Code</a>
+        </div>`
+    );
+};
+
+// Function to create a generic div element
+const createDivElement = (className, innerHTML) => {
+    const div = document.createElement('div');
+    div.className = className;
+    div.innerHTML = innerHTML;
+    return div;
+};
+
+// Function to toggle the visibility of the 'View More' button
+const toggleMoreBtn = (sibling, data) => {
+    const moreBtn = sibling.parentElement.querySelector('#view-more-btn');
+    moreBtn.style.display = (data.length > 6) ? 'block' : 'none';
+};
+
+// Event listeners for 'View More' buttons
+const fetchMoreData = () => {
+    const moreBtns = document.querySelectorAll('#view-more-btn');
+    moreBtns.forEach(moreBtn => {
+        const span = moreBtn.querySelector('span');
+        moreBtn.addEventListener('click', () => {
+            const section = moreBtn.parentElement.classList.contains('skills-section') ? 'skills' : 'projects';
+            const sectionAttr = section === 'skills' ? '#skills-section' : '.projects';
+            (span.textContent.trim() === 'more') ? fetchData(section, sectionAttr, true) : fetchData(section, sectionAttr);
+            span.textContent = (span.textContent.trim() === 'more') ? 'less' : 'more';
+        });
+    });
+};
+
+// Call the functions
+scrollWindow();
+validateForm();
+fetchData('skills', '#skills-section');
+fetchData('projects', '.projects');
+fetchMoreData();
